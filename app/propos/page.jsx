e3f8cb21db4b45398f6ffcbfd96b587a0d9e76a5
Monel Cocou GAFFAN <monelcocou@gmail.com>
2024-08-02 23:39:50 +0100

    feat: Intégration de la page EXPERIENCE

--- app/propos/page.jsx
+++ app/propos/page.jsx
@@ -192,14 +192,46 @@ const Propos = () => {
           <div className="min-h-[70vh] w-full">
             {/*Experience*/}
             <TabsContent value="experience" className="w-full">
-              Experience
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
+
+            {/*Education*/}
             <TabsContent value="education" className="w-full">
               Education
             </TabsContent>
+
+            {/*Skills*/}
             <TabsContent value="skills" className="w-full">
               Skills
             </TabsContent>
+
+            {/*About*/}
             <TabsContent value="about" className="w-full">
               About
             </TabsContent>
