1ce307dfd5edd9aba5c63d4446b626295a3a6724
Monel Cocou GAFFAN <monelcocou@gmail.com>
2024-11-16 05:02:58 +0100

    feat: Finalisation de la page realisations

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,8 +1,20 @@
 "use client";
 import React from "react";
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 
-const WorkSliderBtns = () => {
-  return <div></div>;
+const WorkSliderBtns = ({ containerStyles, btnStyles, iconsStyles }) => {
+  const swiper = useSwiper();
+  return (
+    <div className={containerStyles}>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretLeftBold />
+      </button>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretRightBold />
+      </button>
+    </div>
+  );
 };
 
 export default WorkSliderBtns;
